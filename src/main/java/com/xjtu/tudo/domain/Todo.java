0d772c702c59b5f0338c4588b3055b676471cb99
wangmengxx <13720636683@163.com>
2020-03-22 22:52:39 +0800

    test

---
+++ src/main/java/com/xjtu/tudo/domain/Todo.java
@@ -0,0 +1,32 @@
+package com.xjtu.tudo.domain;
+
+
+public class Todo {
+    private int id;
+    private String content;
+    private String createdTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
+    }
+}
